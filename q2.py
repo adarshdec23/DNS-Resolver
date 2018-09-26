c6375db24c1b1751ebf292dfdcd7a3ddb9f586a2
adarshdec23 <adarshdec23@gmail.com>
2018-09-26 03:55:34 -0400

    working q2

--- q2.py
+++ q2.py
@@ -2,6 +2,7 @@ from dns.resolver import dns
 import dns.query, dns.message
 from config import config
 import sys
+import pdb
 
 
 class Resolver:
@@ -21,6 +22,7 @@ class Resolver:
         self.url_index = -1
         self.url_list = str(self.url).split(".")
         self.ds_stack = []
+        self.ds_start = True  # flag to check "special" root validation
 
     def get_next_url_part(self):
         to_ret = self.url_list[self.url_index:]
@@ -40,18 +42,45 @@ class Resolver:
                     return rrset
         return False
 
-    def validate(self, response, key_response):
-        # Get the RRSIG
-        print("START"*10)
-        print(response.authority[1])
-        print(response.authority[2])
-        print("2")
-        print(key_response.answer[0])
-        dns.dnssec.validate(
-            key_response.answer[0],
-            response.authority[2],
-            {dns.name.from_text("."): response.authority[1]}
-        )
+    def make_ds(self, answer, sec_part_of_url):
+        keyrrset = answer[0]
+        ds = []
+        for rr in keyrrset:
+            ds.append(dns.dnssec.make_ds(sec_part_of_url, rr, 'SHA256'))
+        return ds
+
+    def validate(self, response, key_response, sec_part_of_url):
+        if self.ds_start:
+            self.ds_start = False
+            self.ds_stack.insert(0, response.authority[1][0])
+            print("Doing nothing for root. Ideally check against the locally saved/bootstrapped root keys.")
+            return True
+        # Not root, we have something to validate against
+        if self.ds_stack:
+            parent_ds = self.ds_stack.pop(0)
+            ds_list = self.make_ds(key_response.answer, sec_part_of_url)
+            for ds in ds_list:
+                if ds == parent_ds:
+                    print(ds, "||||", parent_ds)
+                    print("Validated!!!!!!")
+                    return True
+        # We are at the end. Check A record
+        else:
+            try:
+                dns.dnssec.validate(response.answer[0], response.answer[1], {dns.name.from_text(sec_part_of_url): key_response.answer[0]})
+                print("Validated last record")
+                return True
+            except (dns.dnssec.ValidationFailure, Exception) as e:
+                print(e)
+        print("Did not validate")
+        return False
+
+    def is_nsec(self, response):
+        for rrset in response.authority:
+            for rr in rrset:
+                if rr.rdtype == dns.rdatatype.NSEC or rr.rdtype == dns.rdatatype.NSEC3:
+                    if rr.to_text().find("DNSKEY") > -1 or rr.to_text().find("RRSIG"):
+                        return True
         return False
 
     def resolve_iteration(self, url_to_resolve, resolver_list) -> dns.message.Message:
@@ -62,6 +91,9 @@ class Resolver:
             response = dns.query.udp(m, resolver, config.TIMEOUT)
             print(response)
             print("-"*60)
+            if (response.flags & dns.flags.AA) != dns.flags.AA and flagsself.is_nsec(response):
+                print("DNSSEC not supported")
+                exit(0)
             # Make a request for the DNS key
             print("Querying for: ", url_to_resolve)
             url_to_resolve = '.'+url_to_resolve
@@ -70,7 +102,9 @@ class Resolver:
             key_message = dns.message.make_query(dns.name.from_text(sec_url_part), dns.rdatatype.DNSKEY, want_dnssec=True)
             key_response = dns.query.udp(key_message, resolver, config.TIMEOUT)
             print(key_response)
-            self.validate(response, key_response)
+            if not self.validate(response, key_response, sec_url_part):
+                print("DNSSec verification failed")
+                exit(0)
             return response
         print("returning false")
         return False
@@ -93,7 +127,6 @@ class Resolver:
                 for rr in rrset:
                     if rr.rdtype == dns.rdatatype.NS:
                         # We want the A record for this. So we use good ol' recursion
-                        print("*"*100, "Recursing?")
                         res = Resolver(str(rr))
                         resolvers = Resolver.get_ip(res.resolve().answer)
         else:
